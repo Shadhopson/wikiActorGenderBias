32905ebc8417b1b25eb061b41e9856cea1632afc
Shad Hopson <hopson.shad@gmail.com>
2015-09-02 17:18:52 -0400

    Got ready for multiple runs

---
+++ actingData.py
@@ -0,0 +1,7 @@
+genderData = [0, 0, 0, 0, 0, 0]
+#number of men 1
+#number of women 1
+#number of words used for men 641
+#number of words for women 268
+#words per man 641
+ #words per woman 268
--- wikiscraper.py
+++ wikiscraper.py
@@ -20,9 +20,9 @@ from bs4 import BeautifulSoup
 import urllib2
 import time
 import re
-from testActressWords import actressWordList
-from testActorWords import actorWordList
-from testActingData import genderData
+from actressWords import actressWordList
+from actorWords import actorWordList
+from actingData import genderData
 # setting variables we want to go through everything
 menWordFrequency = actorWordList
 womenWordFrequency =actressWordList
@@ -52,10 +52,10 @@ for name in actor_list:
     #print countDay
     wikiUrl = 'https://en.wikipedia.org/wiki/'+name
 #These should be done many times, but here we're accessing the website so we'll need to slow things down
-    if month ==1 and countDay==5:
+    if month ==1 and countDay==5 and (count ==1 or count ==2):
         time.sleep(3)
         wordFrequency = {}
-        '''if len(str(month))<2:
+        if len(str(month))<2:
             strMonth = "0"+str(month)
         else:
             strMonth = str(month)
@@ -67,19 +67,19 @@ for name in actor_list:
         months =["January","February","March","April","May","June","July", "August","September","October","November","December"]
         wordDate =months[month-1]+ " "+ str(countDay)
         print date
-        print wordDate'''
+        print wordDate
         try:
             wikiPage = urllib2.urlopen(wikiUrl)
             wikiHtml =wikiPage.read()
             wikiPage.close()
             wikiSoup = BeautifulSoup(wikiHtml,'lxml')
-            ''' actorBDayText = wikiSoup.find(class_="infobox").get_text()
+            actorBDayText = wikiSoup.find(class_="infobox").get_text()
             if (date) not in actorBDayText:
                 if(wordDate) not in actorBDayText:
                     failSiteList.append("noBD"+ wikiUrl)
                     print actorBDayText
                     continue
-            print "Bday text " +  actorBDayText'''
+            print "Bday text " +  actorBDayText
             actorPar = wikiSoup.find_all('p')
             for p in actorPar:
                 wordList = p.get_text().split()
@@ -137,12 +137,12 @@ print "Man Word Count: " + str(manWordCount)
 print "Woman Word Count: " + str(womanWordCount)
 print "men: " + str(manCount)
 print "women: " + str(womanCount)
-actorWordDoc = open("testactorWords.txt", "wb")
-actressWordDoc = open("testactressWords.txt","wb")
-actingDataDoc = open("testactingData.txt", "wb")
-actorWordDoc.write(str(menWordFrequency))
-actressWordDoc.write(str(womenWordFrequency))
-actingDataDoc.write(str([manCount,womanCount,manWordCount,womanWordCount,manWordCount/manCount,womanWordCount/womanCount])+ "\nnumber of men " + str(manCount) + "\nnumber of women " + str(womanCount) + "\nnumber of words used for men " + str(manWordCount) + "\nnumber of words for women " + str(womanWordCount) + "\nwords per man "+ str(manWordCount/manCount) + "\n words per woman " + str(womanWordCount/womanCount))
+actorWordDoc = open("testActorWords.txt", "wb")
+actressWordDoc = open("testActressWords.txt","wb")
+actingDataDoc = open("testActingData.txt", "wb")
+actorWordDoc.write("actorWordList = " + str(menWordFrequency))
+actressWordDoc.write("actressWordList = " + str(womenWordFrequency))
+actingDataDoc.write("genderData = "+str([manCount,womanCount,manWordCount,womanWordCount,manWordCount/manCount,womanWordCount/womanCount])+ "\n#number of men " + str(manCount) + "\n#number of women " + str(womanCount) + "\n#number of words used for men " + str(manWordCount) + "\n#number of words for women " + str(womanWordCount) + "\n#words per man "+ str(manWordCount/manCount) + "\n #words per woman " + str(womanWordCount/womanCount))
 #The next thing I want to do is make it so that it gets added to a general men's dictionary, or a general women's dictionary.
 actorWordDoc.close()
 actressWordDoc.close()
