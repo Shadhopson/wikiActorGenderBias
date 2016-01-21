7028114bd972fd3d2d4e1d656675f76802bf53d9
Shadhopson <hopson.shad@gmail.com>
2016-01-21 01:46:17 -0500

    Delete actingData.py

--- actingData.py
+++
@@ -1,7 +0,0 @@
-genderData = [2474, 2127, 1226811, 868659, 495, 408]
-#number of men 2474
-#number of women 2127
-#number of words used for men 1226811
-#number of words for women 868659
-#words per man 495
- #words per woman 408
